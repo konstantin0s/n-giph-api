b0b685adc67cb9c1f4e2edd0fb20814cf4da37a8
konstantin0s <constantintofan85@gmail.com>
2020-04-23 18:02:27 +0200

    Updates

---
+++ precache-manifest.5391ef3716cf4e1db4e6e5ec1bea731e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b785a11318e91608552078672bbbbd9",
+    "url": "/n-giph-api/index.html"
+  },
+  {
+    "revision": "26fe95fe22fd1859aacf",
+    "url": "/n-giph-api/static/css/2.cda14bce.chunk.css"
+  },
+  {
+    "revision": "a5bec80952c85acd89df",
+    "url": "/n-giph-api/static/css/main.ed7b92b7.chunk.css"
+  },
+  {
+    "revision": "26fe95fe22fd1859aacf",
+    "url": "/n-giph-api/static/js/2.ded4df5d.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/n-giph-api/static/js/2.ded4df5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5bec80952c85acd89df",
+    "url": "/n-giph-api/static/js/main.8ad3e8aa.chunk.js"
+  },
+  {
+    "revision": "a1c723e02537dc8e61fe",
+    "url": "/n-giph-api/static/js/runtime-main.61fb1afe.js"
+  }
+]);
