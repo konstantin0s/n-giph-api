71b746c056fdae85fea8165759674c3dc0d755a7
konstantin0s <constantintofan85@gmail.com>
2020-04-23 17:07:38 +0200

    Add card from material ui

--- src/components/Gif.js
+++ src/components/Gif.js
@@ -1,22 +1,58 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 import './css/Gif.css';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 140,
+  },
+});
 
  const Gif = (props) => {
+    const classes = useStyles();
 
     return (
         <div className="child-gif">
-                 <li key={props.gif.id}>
-                <img alt="gifff" src={props.gif.images.downsized_large.url} />
-                <h3>{props.gif.title}</h3>
-                <h5> {props.gif.username}</h5>
-                <p>{props.gif.import_datetime}</p>
-                    </li>
-                    <Link className="linkz" id={props.gif.id} to={`/gif/${props.gif.id}`}>
-                   Red More..
-            </Link>
-        </div>
-    )
+      <Card className={classes.root}>
+        <CardActionArea>
+          <CardMedia
+            className={classes.media}
+            image={props.gif.images.downsized_large.url}
+            title="alagif"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              {props.gif.title}
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+           {props.gif.username}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Button size="small" color="primary">
+            Share ?
+          </Button>
+       
+          <Link className="linkx" id={props.gif.id} to={`/gif/${props.gif.id}`}>
+                  See More..
+        </Link>
+        
+        </CardActions>
+      </Card>
+      </div>
+    );
 }
 
 
--- src/components/OneGif.js
+++ src/components/OneGif.js
@@ -2,6 +2,23 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import './css/Gif.css';
 import Loading from './Loading';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles({
+    root: {
+      maxWidth: 345,
+    },
+    media: {
+      height: 140,
+    },
+  });
+
 
 class OneGif extends Component {
 constructor(props) {
@@ -30,18 +47,33 @@ componentDidMount() {
 }
 
 delayRender = () => {
-    const {images, title, username, import_datetime, id} = this.state.gif;
+    const {images, title, username, import_datetime} = this.state.gif;
     console.log(images, title, username);
     const { isLoading} = this.state;
     if (!isLoading) {
 
     return (
-        <li key={id}>
-           <img alt="gifff" src={images.downsized_large.url} />
-           <h3>{title}</h3>
-           {/* <h5> {this.state.gif.username}</h5> */}
-           <p>{import_datetime}</p>
-       </li> 
+    <Card className={useStyles.root}>
+    <CardActionArea>
+      <img alt="gifff" src={images.downsized_large.url} />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="h2">
+          {title}
+        </Typography>
+        <Typography variant="body2" color="textSecondary" component="p">
+       {import_datetime}
+        </Typography>
+      </CardContent>
+    </CardActionArea>
+    <CardActions>
+      <Button size="small" color="primary">
+        Share ?
+      </Button>
+      <Button size="small" color="primary">
+            The end
+      </Button>
+    </CardActions>
+  </Card>
 )
     } else {
         return (
