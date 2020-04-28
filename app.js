137493ccdbf398bbcd070a7d59ce37871cb259d2
konstantin0s <constantintofan85@gmail.com>
2020-04-28 21:39:40 +0200

    prepare to deploy

--- app.js
+++ app.js
@@ -51,17 +51,14 @@ app.use(bodyParser.urlencoded({
       res.status(err.status || 500);
     });
 
-    // const { NODE_ENV = 'production' } = process.env;
-    // const IN_PROD = NODE_ENV === 'production'; 
 
-    if (process.env.NODE_ENV === 'production') {
         // Set static folder
         app.use(express.static('client/build'));
       
         app.get('*', (req, res) => {
           res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
         });
-      }
+      
       
   
     app.listen(process.env.PORT || 5000, function() {
