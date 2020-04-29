9a50522f1b8339a377b9a691f477595833820eda
konstantin0s <constantintofan85@gmail.com>
2020-04-29 17:08:20 +0200

    fix error

--- app.js
+++ app.js
@@ -47,11 +47,11 @@ app.use(function(req, res, next) {
     app.use('/', gif);
   
     // catch 404 and forward to error handler
-  app.use(function(req, res, next) {
-      var err = new Error('Not Found');
-      err.status = 404;
-      next(err);
-    });
+  // app.use(function(req, res, next) {
+  //     var err = new Error('Not Found');
+  //     err.status = 404;
+  //     next(err);
+  //   });
   
     
     // error handler
