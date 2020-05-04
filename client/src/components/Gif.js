0990407f0919cc34c128ce4ae3231dfe657e8c04
konstantin0s <constantintofan85@gmail.com>
2020-05-04 19:07:28 +0200

    Add background color on body & style the fancybox

--- client/src/components/Gif.js
+++ client/src/components/Gif.js
@@ -6,17 +6,19 @@ import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import Sharing from './Sharing';
+import ReactFancyBox from 'react-fancybox';
+import 'react-fancybox/lib/fancybox.css';
 
 const useStyles = makeStyles({
   root: {
     maxWidth: 345,
   },
   media: {
-    height: 140,
+    height: 160,
+    width: 345
   },
 });
 
@@ -27,11 +29,13 @@ const useStyles = makeStyles({
         <div className="child-gif">
       <Card className={classes.root}>
         <CardActionArea>
-          <CardMedia
+          {/* <CardMedia
             className={classes.media}
             image={props.gif.images.downsized_large.url}
             title="alagif"
-          />
+          /> */}
+               <ReactFancyBox className={classes.media} thumbnail={props.gif.images.downsized_large.url}
+                image={props.gif.images.downsized_large.url} />
           <CardContent>
             <Typography gutterBottom variant="h5" component="h2">
               {props.gif.title}
